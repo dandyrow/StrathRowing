bca24cae2d8ecedb514bf2cfbaa0f7dbd04fac5f
Daniel Lowry <dlowry909@gmail.com>
2019-06-02 01:43:05 +0100

    Complete responsive nav & add favicon
    Responsive navbar is now complete, with animations and changing on
    scroll. It correctly resizes and CSS has been refactored to be mobile
    first enabling a reduction in code.
    
    Added 32x32 favicon and created proper res logo.

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,4 @@
+//Hamburger menu for mobile nev
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
@@ -14,3 +15,18 @@ $(window).bind('resize', function() {
 clearTimeout(timer);
 timer = setTimeout(function(){ $(window).resize(); }, 250);
 });
+
+
+//Change nav color on scroll
+$(document).ready(function(){
+  $(window).scroll(function(){
+      var scroll = $(window).scrollTop();
+      if (scroll > 80) {
+        $("nav").css("background" , "var(--accent-color)");
+      }
+
+      else{
+          $("nav").css("background" , "rgba(0, 0, 0, 0)");  	
+      }
+  })
+})
