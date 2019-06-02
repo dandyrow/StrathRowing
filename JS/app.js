dd4f636f87004fab21a6eef2f440591a8d0511db
Daniel Lowry <dlowry909@gmail.com>
2019-06-02 13:51:43 +0100

    Fix hamburger menu animation on mobile

--- JS/app.js
+++ JS/app.js
@@ -2,6 +2,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
+const header = document.querySelector("header")
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
